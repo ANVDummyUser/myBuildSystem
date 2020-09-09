4dec387425872295253ee3c8d1393520cae24789
Vollinger <erik.tisovsky@anv-tech.com>
2020-09-09 14:26:38 +0200

    hgj

--- vars/dummy.groovy
+++ vars/dummy.groovy
@@ -1,4 +1,4 @@
 def call(path) {
 
-	bat "LABVIEWCLI -OperationName RunVI -VIPath \"C:\\Program Files (x86)\\Dummy\\JKI_VI_Tester.vi\" "${path}"" 
+	bat "LABVIEWCLI -OperationName RunVI -VIPath \"C:\\Program Files (x86)\\Dummy\\JKI_VI_Tester.vi\" "${path}" " 
 }
