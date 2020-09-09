d6b684612fe425afa532dfeac6d79d5920312fab
Vollinger <erik.tisovsky@anv-tech.com>
2020-09-09 12:26:08 +0200

    sdf

--- vars/dummy.groovy
+++ vars/dummy.groovy
@@ -1,4 +1,4 @@
 def call(message) {
 
-	bat "LABVIEWCLI -OperationName RunVI -VIPath "C:\Users\ANV\Desktop\JKI_VI_Tester.vi""
+	bat "LABVIEWCLI -OperationName RunVI -VIPath "C:\Users\ANV\Documents\JKI_VI_Tester.vi""
 }
