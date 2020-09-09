3ca09bcd7361084f88a9dc84307fd931b97d9856
Vollinger <erik.tisovsky@anv-tech.com>
2020-09-09 09:46:39 +0200

    egr

--- vars/dummy.groovy
+++ vars/dummy.groovy
@@ -1,4 +1,4 @@
 def call(message) {
 
-	LabVIEWCLI -LogToConsole true -OperationName RunVI -VIPath "C:\Users\ANV\Desktop\JKI_VI_Tester.vi"
+	bat "LabVIEWCLI -LogToConsole true -OperationName RunVI -VIPath "C:\Users\ANV\Desktop\JKI_VI_Tester.vi""
 }
