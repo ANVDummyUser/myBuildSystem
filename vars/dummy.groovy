5b65234f8634aa9f99ff75f8093cb024c93be3cf
Vollinger <erik.tisovsky@anv-tech.com>
2020-09-09 07:01:34 +0200

    added dummy groovy

---
+++ vars/dummy.groovy
@@ -0,0 +1,7 @@
+def call(lvProjectPath, targetName, buildSpecName, lvVersion, lvBitness) {
+	def stepsDir = "${WORKSPACE}\\${BUILD_SYSTEM_REPO}\\steps"
+	def projectPath = "${WORKSPACE}\\${lvProjectPath}"
+		
+	echo 'it`s working'
+
+}
