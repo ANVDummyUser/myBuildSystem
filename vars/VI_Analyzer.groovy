fe7f55584c44f1185713bbf08bf4577c1bae183f
Vollinger <erik.tisovsky@anv-tech.com>
2020-09-10 07:49:24 +0200

    wdqe

--- vars/VI_Analyzer.groovy
+++ vars/VI_Analyzer.groovy
@@ -1,4 +1,4 @@
 def call(Config_Path, Report_Path, Config_Password, Report_Save_Type) {
 
-	bat "LABVIEWCLI -OperationName RunVIAnalyzer -ConfigPath \"${Config_Path}\ -ReportPath \"${Report_Path}\ -ReportSaveType "Report_Save_Type" -ConfigPassword "Config_Password"
+	bat "LABVIEWCLI -OperationName RunVIAnalyzer -ConfigPath \"${Config_Path}\ -ReportPath \"${Report_Path}\ -ReportSaveType {Report_Save_Type} -ConfigPassword {Config_Password}
 }
