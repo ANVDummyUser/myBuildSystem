46ec4d741dcdfaa6e1a7e3c05d258c0427ad5e71
Vollinger <erik.tisovsky@anv-tech.com>
2020-09-14 15:05:13 +0200

    ghng

--- vars/Execute_Build_Spec.groovy
+++ vars/Execute_Build_Spec.groovy
@@ -1,4 +1,4 @@
 def call(Project_Path, Target_Name, Build_Spec_Name) {
 
-	bat "LABVIEWCLI -OperationName ExecuteBuildSpec -ProjectPath \"${Project_Path}\" -TargetName {Target_Name} -BuildSpecName {Build_Spec_Name} "
+	bat "LABVIEWCLI -OperationName ExecuteBuildSpec -ProjectPath \"${Project_Path}\" -TargetName \"${Target_Name}\" -BuildSpecName \"${Build_Spec_Name}\" "
 }
