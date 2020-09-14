bf2bfa6bd66553f0b0a90a7e07ed2cab7c338f90
Vollinger <erik.tisovsky@anv-tech.com>
2020-09-14 15:56:38 +0200

    dw

--- vars/Execute_Build_Spec.groovy
+++ vars/Execute_Build_Spec.groovy
@@ -1,6 +1,6 @@
-def call(Project_Path, Target_Name, Build_Spec_Name, LabVIEW_Path) {
-	
-	def projectPath = "C:\\Program Files (x86)\\National Instruments\\LabVIEW 2020\\LabVIEW.exe"
+def call(Project_Path, Target_Name, Build_Spec_Name, LabVIEW_Version) {
+	def LabVIEW_Dir = "C:\\Program Files (x86)\\National Instruments\\LabVIEW "
+	def projectPath = "${LabVIEW_Dir}${LabVIEW_Version}\\LabVIEW.exe"
 	
 	bat "LABVIEWCLI -OperationName ExecuteBuildSpec -ProjectPath \"${Project_Path}\" -TargetName \"${Target_Name}\" -BuildSpecName \"${Build_Spec_Name}\" -LabVIEWPath \"${projectPath}\" "
 }
